a4b1338b7a63e8635d320d3da580c3109726f739
Lyne Lin <lyne.lin@emc.com>
2017-03-27 23:05:48 -0400

    add support for cancelled graph

--- src/monorail/views/App.js
+++ src/monorail/views/App.js
@@ -8,17 +8,12 @@ import merge from 'lodash/merge';
 
 import Messenger from 'src-common/lib/Messenger';
 import config from 'src-config';
-import RackHDRestAPIv2_0 from 'src-common/messengers/RackHDRestAPIv2_0';
 import ProgressEventsMessenger from 'src-common/messengers/ProgressEventsMessenger';
 import AppContainer from 'src-common/views/AppContainer';
 import GraphProgressTable from './GraphProgressTable';
-import EventTable from './EventTable';
 import {LinearProgress, AppBar} from 'material-ui';
 import {
-    FlatButton,
-    DropDownMenu,
-    MenuItem,
-    TextField
+    FlatButton
 } from 'material-ui';
 
 export default class App extends Component {
@@ -29,41 +24,49 @@ export default class App extends Component {
         let self = this;
 
         self.tableHeaderSent = true;
-        self.userMap = ['heartbeat', 'polleralert', 'graph', 'node'];
-        self.messagesEnd;
 
-        self.eventList = [];
+        self.graphProgressCollection = [];
+        self.graphFinished = [];
 
         console.log("Starting to monitor websocket");
-        self.events = new Messenger('text', config.MonoRail_WSS);
+        self.events = new ProgressEventsMessenger();
         self.events.connect();
-        self.events.on('message', rcv => {
-
-            let newState = {};
+        self.events.listen(msg => {
 
             if(! self.state.hasFirstEvent) {
-                newState.hasFirstEvent = true;
-                self.eventList = [];
+                self.setState({hasFirstEvent: true});
             }
 
-            let exp = new RegExp('^'+self.userMap[self.state.userOption - 1]);
-            if ((rcv.deliveryInfo.routingKey.match(exp)) &&
-                (rcv.msg.hasOwnProperty('version'))){
-                console.log('valid msg', rcv.msg);
-                self.eventList.push(rcv.msg);
-                newState.events = self.eventList;
-                self.setState(newState);
-                self.scrollToBottom();
-            }
+            console.log(msg);
+
+            //let action = self.checkEventAction(msg);
+            //console.log(action);
+
+            //if (action === 'progress')
+            //{
+            self.decodeProgress(msg.data.data);
+            //} else if (action === 'finished') {
+                //self.decodeFinish(msg.data.typeId, msg.data.data);
+            //}
+
+            console.log('-----graphProgressCollection', self.graphProgressCollection);
+
+            self.setState(self.graphProgressCollection);
         });
 
-        return self;
+        return this;
     }
 
     state = {
-        userOption: 2,
         hasFirstEvent: false,
-        events: {}
+        graphProgress : 0,
+        graphDesc: '',
+        graphName: '',
+        graphId: '',
+        taskProgress: 0,
+        taskName: '',
+        taskId: '',
+        taskDesc: ''
     }
 
     static contextTypes = {
@@ -71,15 +74,128 @@ export default class App extends Component {
         router: PropTypes.any
     }
 
-    scrollToBottom = () => {
-        const node = findDOMNode(this.messagesEnd);
-        node.scrollIntoView({behavior: "smooth"});
+    checkEventAction = (msg) => {
+        console.log(msg.id);
+        let exp = new RegExp('graph.started');
+        if ((msg.id.match(exp))) {
+            return "started";
+        }
+
+        exp = new RegExp('graph.finished.information');
+        if ((msg.id.match(exp))) {
+            return "finished";
+        }
+
+        exp = new RegExp('graph.progress');
+        if ((msg.id.match(exp))) {
+            return "progress";
+        }
+
+        return "ignore";
+    }
+
+    decodeFinish = (graphId, payload) => {
+        var self = this;
+        if (!self.graphProgressCollection.hasOwnProperty(graphId)) {
+            return;
+        }
+        self.graphProgressCollection[graphId].graphStatus = payload.status;
+        console.log(graphId);
+        console.log(self.graphProgressCollection[graphId].graphStatus);
+    }
+
+    decodeProgress = (payload) => {
+
+        var self = this;
+
+        let graphId = payload.graphId;
+
+        if (self.graphFinished.indexOf(graphId) > 0) {
+            return;
+        }
+
+        // Get graph info
+        if (!self.graphProgressCollection.hasOwnProperty(graphId)) {
+            self.graphProgressCollection[graphId] = {
+                graphId: payload.graphId,
+                graphDesc: payload.progress.description,
+                graphName: payload.graphName,
+                graphProgress: parseInt(payload.progress.percentage),
+                tasks: {}
+            };
+        }
+        else {
+            self.graphProgressCollection[graphId].graphDesc =
+                payload.progress.description;
+            self.graphProgressCollection[graphId].graphProgress =
+                parseInt(payload.progress.percentage);
+        }
+
+        console.log(payload.progress.description);
+        if (self.graphProgressCollection[graphId].graphProgress < 100) {
+            self.graphProgressCollection[graphId].graphStatus = "running";
+        }
+        else if (payload.progress.description.match(new RegExp('cancelled'))){
+            self.graphProgressCollection[graphId].graphStatus = "cancelled";
+            for (var key in self.graphProgressCollection[graphId].tasks) {
+                self.graphProgressCollection[graphId].tasks[key].taskStatus = "cancelled";
+            }
+            self.graphFinished.push(graphId);
+            return;
+        }
+        else {
+            self.graphProgressCollection[graphId].graphStatus = "succeeded";
+        }
+
+        for (var key in self.graphProgressCollection[graphId].tasks) {
+            if (key.match(/TBD*/)){
+                delete self.graphProgressCollection[graphId].tasks[key];
+            }
+        }
+
+        if (!payload.hasOwnProperty("taskProgress")) {
+            return;
+        }
+
+        let taskId = payload.taskProgress.taskId;
+
+        if (!self.graphProgressCollection[graphId].tasks.hasOwnProperty(taskId)) {
+            self.graphProgressCollection[graphId].tasks[taskId] = {
+                taskName: payload.taskProgress.taskName,
+                taskProgress: parseInt(payload.taskProgress.progress.percentage),
+                taskDesc: payload.taskProgress.progress.description
+            };
+        }
+        else {
+            self.graphProgressCollection[graphId].tasks[taskId].taskDesc =
+                payload.taskProgress.progress.description;
+            self.graphProgressCollection[graphId].tasks[taskId].taskProgress =
+                payload.taskProgress.progress.hasOwnProperty("percentage")?
+                parseInt(payload.taskProgress.progress.percentage):
+                Math.floor(100*payload.taskProgress.progress.value/payload.taskProgress.progress.maximum);
+        }
+
+        if (self.graphProgressCollection[graphId].tasks[taskId].taskProgress < 100) {
+            self.graphProgressCollection[graphId].tasks[taskId].taskStatus = "running";
+        }
+        else {
+            self.graphProgressCollection[graphId].tasks[taskId].taskStatus = "succeeded";
+        }
+
+        //var count = Object.keys(self.graphProgressCollection[graphId].tasks).length;
+        //console.log("!!!!", count);
+        for (let i = payload.progress.value; i < payload.progress.maximum; i ++) {
+            self.graphProgressCollection[graphId].tasks["TBD "+(i+1)]={
+                taskStatus: "pending",
+                taskProgress: 0
+            };
+        }
     }
 
     renderHeader = () => {
         return (
             <AppBar
-                title="Tools for demo event format"
+                title="Tools for demo graph progress from AMQP event"
             />
         )
     }
@@ -89,60 +205,45 @@ export default class App extends Component {
 
         if(! self.state.hasFirstEvent) {
             return (
-                <div style={{clear:"both"}}>
+                <div>
                     <FlatButton label="No notification arrive yet"/>
                 </div>
             )
         }
+        let graphProgressElements = [];
+
+        let showHeader = true;
+
+        Object.keys(self.graphProgressCollection).forEach(function(graphId){
+
+            let graphState = self.state[graphId];
+
+            graphProgressElements.push(
+                <div>
+                    <GraphProgressTable
+                        key={graphId}
+                        showHeader={showHeader}
+                        graphData={graphState}
+                    />
+                </div>
+            )
+
+            showHeader = false;
+        });
 
         return (
-            <div style={{clear:"both"}}>
-                <EventTable
-                    eventItems={self.state.events}
-                />
+            <div>
+                {graphProgressElements}
             </div>
         )
-    }
 
-    handleUserOption = (event, index, val) => {
-        this.setState({userOption: val, hasFirstEvent: false});
-    }
-
-    renderDropDownMenu = () => {
-        return (
-            <div style={{float:"left"}}>
-                <DropDownMenu value={this.state.userOption} onChange={this.handleUserOption}>
-                    <MenuItem value={1} primaryText="heartbeat" />
-                    <MenuItem value={2} primaryText="polleralert" />
-                    <MenuItem value={3} primaryText="graph" />
-                    <MenuItem value={4} primaryText="node" />
-                </DropDownMenu>
-            </div>
-        )
-    }
-
-    renderAmqpInfo = () => {
-        var tmpStr = "routingKey: " + this.userMap[this.state.userOption - 1] +
-                ".<action>.<severity>.<typeId>.<nodeId>"
-        return (
-            <div style={{clear:"both"}}>
-                <TextField
-                    fullWidth={true}
-                    value={tmpStr} />
-            </div>
-        )
     }
 
     render(){
         return (
             <AppContainer key="app">
                 {this.renderHeader()}
-                {this.renderDropDownMenu()}
-                {this.renderAmqpInfo()}
                 {this.renderContent()}
-                <div style={{ float:"left", clear: "both" }}
-                    ref={(el) => { this.messagesEnd = el; }}>
-                </div>
             </AppContainer>
         )
     }
